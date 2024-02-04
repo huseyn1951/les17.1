42e75bc05ad08c7b50aff7304a95af5a83443cc8
huseyn1951 <sultanlkhuseyn@gmail.com>
2024-02-04 23:33:38 +0400

    faculty

---
+++ ConsoleApp21/Faculty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Faculty
+    {
+        public static int Count { get; set; }
+        public string Name { get; set; }
+
+        public Faculty()
+        {
+            Count++;
+        }
+    }
+}
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faculty.Count = 99;
+            Faculty grapichDesign = new Faculty();
+            grapichDesign.Name = "grapich";
+            char[] nameArray1 = grapichDesign.Name.ToCharArray();
+            Console.WriteLine(nameArray1[0] +""+ Faculty.Count);
+
+            Faculty programming = new Faculty();
+            programming.Name = "programming";
+            char[] nameArray2 = programming.Name.ToCharArray();
+            Console.WriteLine(nameArray2[0]+""+Faculty.Count);
+
+            Faculty digitalMarketing = new Faculty();
+            digitalMarketing.Name = "digitalmarketing";
+            char[] nameArray3 = digitalMarketing.Name.ToCharArray();
+            Console.WriteLine(nameArray3[0] + "" + Faculty.Count);
+        }
+    }
+}
